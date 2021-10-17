89e741948ef9929c41b2a56ca44760284fef531b
Kody Deda <kodydeda4@gmail.com>
2021-10-17 11:36:31 -0400

    add MUI to FireTodos

--- src/components/FireTodos.tsx
+++ src/components/FireTodos.tsx
@@ -1,3 +1,19 @@
+import { Logout } from "@mui/icons-material";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  Checkbox,
+  Container,
+  List,
+  ListItem,
+  Stack,
+  TextField
+} from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import {
   addDoc,
   deleteDoc,
@@ -8,13 +24,106 @@ import {
   query,
   serverTimestamp,
   updateDoc,
-  where,
+  where
 } from "firebase/firestore";
 import React, { useEffect } from "react";
 import { firestore } from "../config/firebase";
 import Todo from "../types/Todo";
 
 export default function FireTodos() {
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+
+  const updateTodos = React.useEffect(() => {
+    onSnapshot(
+      // query(firestore.todos2, orderBy("timestamp", "desc")),
+      query(firestore.todos2),
+      (snapshot) =>
+        setTodos(
+          snapshot.docs.map((doc: any) => ({ id: doc.id, ...doc.data() }))
+        )
+    );
+  }, []);
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Container>
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Firetodos
+              </Typography>
+              <Stack direction="row" spacing={2}>
+                <Button
+                  onClick={() => addTodo()}
+                  color="inherit"
+                  variant="outlined"
+                  startIcon={<AddIcon />}
+                >
+                  Add
+                </Button>
+                <Button
+                  onClick={() => clearAll()}
+                  color="inherit"
+                  variant="outlined"
+                  startIcon={<DeleteIcon />}
+                >
+                  Clear All
+                </Button>
+                <Button
+                  onClick={() => clearDone()}
+                  color="inherit"
+                  variant="outlined"
+                  startIcon={<DeleteIcon />}
+                >
+                  Clear Done
+                </Button>
+                <Button
+                  // onClick={() => viewStore.action.logout()}
+                  color="inherit"
+                  variant="outlined"
+                  startIcon={<Logout />}
+                >
+                  Logout
+                </Button>
+              </Stack>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Box>
+      <List
+        sx={{
+          bgcolor: "background.paper",
+          overflow: "auto",
+          height: "100vh",
+        }}
+      >
+        <Container>
+          {todos.map((todo: Todo) => (
+            <ListItem>
+              <Checkbox
+                onClick={() => toggleDone(todo)}
+                edge="start"
+                checked={todo.done}
+              />
+              <TextField
+                placeholder={todo.text}
+                autoFocus
+                variant="standard"
+                fullWidth
+              />
+              {/* {todo.text} {todo.id} */}
+            </ListItem>
+          ))}
+        </Container>
+      </List>
+    </>
+  );
+}
+
+// -----------------------------------------------------------------------------------------------------------------
+
+const Todos = () => {
   const [todos, setTodos] = React.useState<Todo[]>([]);
   const queryText = "3";
 
@@ -36,7 +145,7 @@ export default function FireTodos() {
       <ul>
         {todos.map((todo: Todo) => (
           <li key={todo.id}>
-            <button onClick={() => updateTodo(todo)}>edit</button>
+            <button onClick={() => toggleDone(todo)}>edit</button>
             <button onClick={() => deleteTodo(todo)}>delete</button>
             <span
               style={{
@@ -54,7 +163,7 @@ export default function FireTodos() {
       </ul>
     </>
   );
-}
+};
 
 // -----------------------------------------------------------------------------------------------------------------
 // Actions
@@ -71,10 +180,10 @@ const deleteTodo = async (todo: Todo) => {
   await deleteDoc(doc(firestore.todos2, todo.id));
 };
 
-const updateTodo = async (todo: Todo) => {
+const toggleDone = async (todo: Todo) => {
   updateDoc(doc(firestore.todos2, todo.id), {
-    text: "updated",
-    done: true,
+    text: todo.text,
+    done: !todo.done,
     timestamp: serverTimestamp(),
   });
 };
