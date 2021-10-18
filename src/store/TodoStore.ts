f183a9698daaf8ffac0d7dab4d95f5268af6c6f6
Kody Deda <kodydeda4@gmail.com>
2021-10-18 09:11:06 -0400

    update Todo and Auth store's without splitting form Root

--- src/store/TodoStore.ts
+++ src/store/TodoStore.ts
@@ -1,21 +1,21 @@
-import {
-  createStore,
-  Thunk,
-  thunk
-} from "easy-peasy";
+import { auth } from "./../config/firebase";
+import { getAdditionalUserInfo, User } from "@firebase/auth";
+import { createStore, Thunk, thunk } from "easy-peasy";
 import {
   addDoc,
   deleteDoc,
   doc,
-  getDocs, query,
+  getDocs,
+  query,
   serverTimestamp,
   updateDoc,
-  where
+  where,
 } from "firebase/firestore";
 import { firestore } from "../config/firebase";
 import Todo from "../types/Todo";
 
 interface TodoState {
+  user: User;
   // todos: Todo[];
 }
 
@@ -33,45 +33,53 @@ interface TodoThunks {
 export interface TodoStore extends TodoState, TodoAction, TodoThunks {}
 
 export const todoStore = createStore<TodoStore>({
+  user: auth.currentUser!,
   // todos: [],
-  createTodo: thunk(async () => {
+  createTodo: thunk(async (actions, payload, { getState }) => {
     await addDoc(firestore.todos2, {
       text: "untitled",
       done: false,
       timestamp: serverTimestamp(),
+      userID: getState().user!.uid,
     });
   }),
-  deleteTodo: thunk(async (_, payload) => {
+  deleteTodo: thunk(async (actions, payload) => {
     await deleteDoc(doc(firestore.todos2, payload.id));
   }),
-  toggleTodoDone: thunk(async (_, payload) => {
+  toggleTodoDone: thunk(async (actions, payload) => {
     await updateDoc(doc(firestore.todos2, payload.id), {
       text: payload.text,
       done: !payload.done,
       timestamp: serverTimestamp(),
     });
   }),
-  updateTodoText: thunk(async (_, payload) => {
+  updateTodoText: thunk(async (actions, payload) => {
     updateDoc(doc(firestore.todos2, payload.todo.id), {
       text: payload.text,
       done: payload.todo.done,
       timestamp: serverTimestamp(),
     });
   }),
-  clearAll: thunk(async () => {
-    await getDocs(firestore.todos2).then((snapshot) => {
+  clearAll: thunk(async (actions, payload, { getState }) => {
+    await getDocs(
+      query(firestore.todos2, where("userID", "==", getState().user.uid))
+    ).then((snapshot) => {
       snapshot.docs.forEach(async (document) => {
         await deleteDoc(doc(firestore.todos2, document.id));
       });
     });
   }),
-  clearDone: thunk(async () => {
-    await getDocs(query(firestore.todos2, where("done", "==", true))).then(
-      (snapshot) => {
-        snapshot.docs.forEach(async (document) => {
-          await deleteDoc(doc(firestore.todos2, document.id));
-        });
-      }
-    );
+  clearDone: thunk(async (actions, payload, { getState }) => {
+    await getDocs(
+      query(
+        firestore.todos2,
+        where("userID", "==", getState().user.uid),
+        where("done", "==", true)
+      )
+    ).then((snapshot) => {
+      snapshot.docs.forEach(async (document) => {
+        await deleteDoc(doc(firestore.todos2, document.id));
+      });
+    });
   }),
 });
