bc6695b9dcddfb01978e38df48beff78786b81bb
Kody Deda <kodydeda4@gmail.com>
2021-10-17 20:07:30 -0400

    make rootStore persistent

--- src/store/RootStore.ts
+++ src/store/RootStore.ts
@@ -1,17 +1,18 @@
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  User
+  User,
 } from "@firebase/auth";
-import { Action, action, createStore, Thunk, thunk } from "easy-peasy";
+import { Action, action, createStore, persist, Thunk, thunk } from "easy-peasy";
 import {
   addDoc,
   deleteDoc,
   doc,
-  getDocs, query,
+  getDocs,
+  query,
   serverTimestamp,
   updateDoc,
-  where
+  where,
 } from "firebase/firestore";
 import { auth, firestore } from "../config/firebase";
 import { AlertState, Severity } from "../types/AlertState";
@@ -45,87 +46,93 @@ interface RootThunk {
 
 export interface RootStore extends RootState, RootAction, RootThunk {}
 
-export const rootStore = createStore<RootStore>({
-  // AUTH
-  user: undefined,
-  alert: undefined,
-  setUser: action((state, payload) => {
-    state.user = payload;
-  }),
-  signUp: thunk(async (actions, payload) => {
-    await createUserWithEmailAndPassword(auth, payload.email, payload.password)
-      .then((userCredential) => {
-        actions.setAlert({
-          severity: Severity.success,
-          message: `Successfully created user with email: ${userCredential.user.email}`,
+export const rootStore = createStore<RootStore>(
+  persist({
+    // AUTH
+    user: undefined,
+    alert: undefined,
+    setUser: action((state, payload) => {
+      state.user = payload;
+    }),
+    signUp: thunk(async (actions, payload) => {
+      await createUserWithEmailAndPassword(
+        auth,
+        payload.email,
+        payload.password
+      )
+        .then((userCredential) => {
+          actions.setAlert({
+            severity: Severity.success,
+            message: `Successfully created user with email: ${userCredential.user.email}`,
+          });
+        })
+        .catch((error) => {
+          actions.setAlert({
+            severity: Severity.error,
+            message: "Please make sure your email and password are incorrect.",
+          });
         });
-      })
-      .catch((error) => {
-        actions.setAlert({
-          severity: Severity.error,
-          message: "Please make sure your email and password are incorrect.",
+    }),
+    signIn: thunk(async (actions, payload) => {
+      await signInWithEmailAndPassword(auth, payload.email, payload.password)
+        .then((userCredential) => {
+          actions.setUser(userCredential.user);
+        })
+        .catch((error) => {
+          actions.setAlert({
+            severity: Severity.error,
+            message: "Please make sure your email and password are incorrect.",
+          });
         });
+    }),
+    signOut: thunk(async (actions) => {
+      auth.signOut();
+      actions.setUser(undefined);
+    }),
+    setAlert: action((state, payload) => {
+      state.alert = payload;
+    }),
+
+    // TODO
+    createTodo: thunk(async () => {
+      await addDoc(firestore.todos2, {
+        text: "untitled",
+        done: false,
+        timestamp: serverTimestamp(),
       });
-  }),
-  signIn: thunk(async (actions, payload) => {
-    await signInWithEmailAndPassword(auth, payload.email, payload.password)
-      .then((userCredential) => {
-        actions.setUser(userCredential.user);
-      })
-      .catch((error) => {
-        actions.setAlert({
-          severity: Severity.error,
-          message: "Please make sure your email and password are incorrect.",
-        });
+    }),
+    deleteTodo: thunk(async (_, payload) => {
+      await deleteDoc(doc(firestore.todos2, payload.id));
+    }),
+    toggleTodoDone: thunk(async (_, payload) => {
+      await updateDoc(doc(firestore.todos2, payload.id), {
+        text: payload.text,
+        done: !payload.done,
+        timestamp: serverTimestamp(),
       });
-  }),
-  signOut: thunk(async (actions) => {
-    auth.signOut();
-    actions.setUser(undefined);
-  }),
-  setAlert: action((state, payload) => {
-    state.alert = payload;
-  }),
-  
-  // TODO
-  createTodo: thunk(async () => {
-    await addDoc(firestore.todos2, {
-      text: "untitled",
-      done: false,
-      timestamp: serverTimestamp(),
-    });
-  }),
-  deleteTodo: thunk(async (_, payload) => {
-    await deleteDoc(doc(firestore.todos2, payload.id));
-  }),
-  toggleTodoDone: thunk(async (_, payload) => {
-    await updateDoc(doc(firestore.todos2, payload.id), {
-      text: payload.text,
-      done: !payload.done,
-      timestamp: serverTimestamp(),
-    });
-  }),
-  updateTodoText: thunk(async (_, payload) => {
-    updateDoc(doc(firestore.todos2, payload.todo.id), {
-      text: payload.text,
-      done: payload.todo.done,
-      timestamp: serverTimestamp(),
-    });
-  }),
-  clearAll: thunk(async () => {
-    await getDocs(firestore.todos2).then((snapshot) => {
-      snapshot.docs.forEach(async (document) => {
-        await deleteDoc(doc(firestore.todos2, document.id));
+    }),
+    updateTodoText: thunk(async (_, payload) => {
+      updateDoc(doc(firestore.todos2, payload.todo.id), {
+        text: payload.text,
+        done: payload.todo.done,
+        timestamp: serverTimestamp(),
       });
-    });
-  }),
-  clearDone: thunk(async () => {
-    await getDocs(query(firestore.todos2, where("done", "==", true))).then(
-      (snapshot) => {
+    }),
+    clearAll: thunk(async () => {
+      await getDocs(firestore.todos2).then((snapshot) => {
         snapshot.docs.forEach(async (document) => {
           await deleteDoc(doc(firestore.todos2, document.id));
         });
-      }
-    );
-  }),
-});
+      });
+    }),
+    clearDone: thunk(async () => {
+      await getDocs(query(firestore.todos2, where("done", "==", true))).then(
+        (snapshot) => {
+          snapshot.docs.forEach(async (document) => {
+            await deleteDoc(doc(firestore.todos2, document.id));
+          });
+        }
+      );
+    }),
+  })
+);
