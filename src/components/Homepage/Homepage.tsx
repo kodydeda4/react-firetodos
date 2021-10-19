857ff32a592d6efe3222d0bcb06a025f24bdd041
Kody Deda <kodydeda4@gmail.com>
2021-10-19 10:56:59 -0400

    cleanup homepage logic

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 import ROUTES from "../../routes";
 import { storeHooks } from "../../store";
-import AppHeader from "./AppHeader";
 import Todos from "./Todos";
 
 export default function Homepage() {
-  if (!storeHooks.useStoreState((state) => state.authModel.user)) {
-    return <Redirect to={ROUTES.login} push={true} />;
-  } else {
-    return <Todos />;
-  }
+  return !storeHooks.useStoreState((state) => state.authModel.user) 
+    ? (<Redirect to={ROUTES.login} push={true} />)
+    : (<Todos />);
 }
