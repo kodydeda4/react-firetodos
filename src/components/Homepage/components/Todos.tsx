dbd482c8f56a0163c87513feacb013275350823b
Kody Deda <kodydeda4@gmail.com>
2021-10-21 08:41:40 -0400

    clean profileview

--- src/components/Homepage/components/Todos.tsx
+++ src/components/Homepage/components/Todos.tsx
@@ -1,11 +1,6 @@
 import { User } from "@firebase/auth";
 import AddIcon from "@mui/icons-material/Add";
-import {
-  Checkbox,
-  Container,
-  List,
-  ListItem, TextField
-} from "@mui/material";
+import { Checkbox, Container, List, ListItem, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import { onSnapshot, query, where } from "firebase/firestore";
 import React from "react";
@@ -36,38 +31,36 @@ export default function Homepage() {
           height: "500",
         }}
       >
-        {/* <Container> */}
-          {/* {viewStore.state.todos.map((todo: Todo) => ( */}
-          {todos.map((todo: Todo) => (
-            <ListItem>
-              <Checkbox
-                onClick={() => viewStore.actions.toggleTodoDone(todo)}
-                edge="start"
-                checked={todo.done}
-              />
-              <TextField
-                placeholder={todo.text}
-                autoFocus
-                variant="standard"
-                fullWidth
-                onChange={(event) =>
-                  viewStore.actions.updateTodoText({
-                    todo: todo,
-                    text: event.target.value,
-                  })
-                }
-              />
-              <Button
-                onClick={() => viewStore.actions.deleteTodo(todo)}
-                color="inherit"
-                variant="outlined"
-                startIcon={<AddIcon />}
-              >
-                Delete
-              </Button>
-            </ListItem>
-          ))}
-        {/* </Container> */}
+        {/* {viewStore.state.todos.map((todo: Todo) => ( */}
+        {todos.map((todo: Todo) => (
+          <ListItem>
+            <Checkbox
+              onClick={() => viewStore.actions.toggleTodoDone(todo)}
+              edge="start"
+              checked={todo.done}
+            />
+            <TextField
+              placeholder={todo.text}
+              autoFocus
+              variant="standard"
+              fullWidth
+              onChange={(event) =>
+                viewStore.actions.updateTodoText({
+                  todo: todo,
+                  text: event.target.value,
+                })
+              }
+            />
+            <Button
+              onClick={() => viewStore.actions.deleteTodo(todo)}
+              color="inherit"
+              variant="outlined"
+              startIcon={<AddIcon />}
+            >
+              Delete
+            </Button>
+          </ListItem>
+        ))}
       </List>
     </>
   );
