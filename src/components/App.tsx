4ac54ea35d5f9202b0d08baaf32693325a7708a3
Kody Deda <kodydeda4@gmail.com>
2021-10-25 10:37:30 -0400

    add hasPremium as snapshot listener.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,42 +1,71 @@
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/system";
 import { StoreProvider } from "easy-peasy";
-import React from "react";
-import {
-  HashRouter, Route, Switch
-} from "react-router-dom";
+import { collection, getFirestore, onSnapshot, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import { firestore } from "../config/firebase";
+import { stripeConfig } from "../config/stripe";
+
 import useAppTheme from "../hooks/useAppTheme";
 import ROUTES from "../routes";
-import { store } from "../store";
+import { store, storeHooks } from "../store";
 import ForgotPassword from "./Authpage/ForgotPassword";
 import Login from "./Authpage/Login";
 import Signup from "./Authpage/Signup";
-import Profile from "./Profile";
 import NotFound from "./NotFound";
-import PrivateRoute from "./_helpers/PrivateRoute";
+import Profile from "./Profile";
 import Homepage from "./Todos";
+import PrivateRoute from "./_helpers/PrivateRoute";
 
 export default function App() {
   return (
     <StoreProvider store={store}>
       <ThemeProvider theme={useAppTheme()}>
         <CssBaseline />
-        <HashRouter>
-          <Switch>
-            {/* Standard */}
-            <Route exact path={ROUTES.login} component={Login} />
-            <Route exact path={ROUTES.signup} component={Signup} />
-            <Route exact path={ROUTES.forgotPassword} component={ForgotPassword}/>
-
-            {/* Private */}
-            <PrivateRoute exact path={ROUTES.home} component={Homepage} />
-            <PrivateRoute exact path={ROUTES.profile} component={Profile} />
-
-            {/* Default */}
-            <Route component={NotFound} />
-          </Switch>
-        </HashRouter>
+        <Routez />
       </ThemeProvider>
     </StoreProvider>
   );
 }
+
+const Routez = () => {
+  const viewStore = {
+    state: storeHooks.useStoreState((state) => state.todoModel),
+    actions: storeHooks.useStoreActions((action) => action.todoModel),
+  };
+  const user = storeHooks.useStoreState((state) => state.authModel.user);
+
+  // update todoslist
+  useEffect(() => {
+    onSnapshot(
+      query(firestore.todos2, where("userID", "==", user?.uid ?? "")),
+      (snapshot) => {
+        viewStore.actions.setTodos(
+          snapshot.docs.map((doc: any) => ({ id: doc.id, ...doc.data() }))
+        );
+        console.log(snapshot)
+      }
+    );
+  }, [user?.uid, viewStore.actions]);
+
+
+
+  return (
+    <HashRouter>
+      <Switch>
+        {/* Standard */}
+        <Route exact path={ROUTES.login} component={Login} />
+        <Route exact path={ROUTES.signup} component={Signup} />
+        <Route exact path={ROUTES.forgotPassword} component={ForgotPassword} />
+
+        {/* Private */}
+        <PrivateRoute exact path={ROUTES.home} component={Homepage} />
+        <PrivateRoute exact path={ROUTES.profile} component={Profile} />
+
+        {/* Default */}
+        <Route component={NotFound} />
+      </Switch>
+    </HashRouter>
+  );
+};
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,7 +1,45 @@
+import {
+  collection,
+  getFirestore, onSnapshot,
+  query
+} from "firebase/firestore";
+import { useEffect } from "react";
+import { stripeConfig } from "../../config/stripe";
+import { storeHooks } from "../../store";
 import ProfileAppHeader from "./ProfileAppHeader";
 import ProfileView from "./ProfileView";
 
 export default function Profile() {
+  const viewStore = {
+    state: storeHooks.useStoreState((state) => state.authModel),
+    actions: storeHooks.useStoreActions((action) => action.authModel),
+  };
+
+  // set has premium
+  useEffect(() => {
+    onSnapshot(
+      query(
+        collection(
+          getFirestore(),
+          "users",
+          viewStore.state.user!.uid,
+          "payments"
+        )
+      ),
+      (snapshot) => {
+        const priceIDs = snapshot.docs.flatMap((doc) => {
+          const items = doc.data().items;
+          const prices = items.map((item: any) => item.price);
+          const ids = prices.map((price: any) => price.id);
+          return ids;
+        });
+        viewStore.actions.setHasPremium(
+          priceIDs.includes(stripeConfig.prices.premium)
+        );
+      }
+    );
+  }, []);
+
   return (
     <>
       <ProfileAppHeader />
