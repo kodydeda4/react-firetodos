cfdbf266d589794f0ef05a8ef1e5026390a8f462
Kody Deda <kodydeda4@gmail.com>
2021-10-27 09:28:26 -0400

    change color

--- src/hooks/useAppTheme.ts
+++ src/hooks/useAppTheme.ts
@@ -2,13 +2,26 @@ import { useMediaQuery } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import React from "react";
 
+// Customize MUI Theme:
+// https://mui.com/customization/palette/
+
 export default function useAppTheme() {
   const mode = useSystemMode();
 
   return createTheme({
     palette: {
       mode,
-    },
+      primary: {
+        main: '#ffa726',
+        light: '#ffb74d',
+        dark: '#f57c00',
+      },
+      secondary: {
+        main: '#f44336',
+        light: '#e57373',
+        dark: '#d32f2f',
+      }
+    },  
   });
 }
 
